2858f9f79461e405d11f142dff927e3d9e2df765
jayasri-ramineedi <rjayasri1707@gmail.com>
2026-06-19 20:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,131 +1,87 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 import joblib
 
-# Load Model
-model = joblib.load("stroke_prediction_model.pkl")
-
-# FastAPI App
 app = FastAPI(
-    title="Stroke Prediction API",
+    title ="🧠 Stroke Risk Prediction System",
     description="""
     Machine Learning API for Stroke Risk Prediction.
 
     Features:
-    - Missing Value Handling
-    - One-Hot Encoding
-    - Robust Scaling
+    - Symptom-Based Stroke Risk Evaluation
+    - Age-Based Stroke Analysis
+    - Data Cleaning & Duplicate Removal
+    - Standard Scaling for Age Feature
     - Logistic Regression Model
-    - Stroke Risk Probability
+    - Stroke Risk Probability Prediction
     """,
-    version="1.0.0"
-)
+    version="1.0.0"  )
+model = joblib.load("stroke_prediction_model.pkl")
 
-# Input Schema
 class Stroke(BaseModel):
-    age: int
-    hypertension: int
-    heart_disease: int
-    avg_glucose_level: float
-    bmi: float
-    avg_RestingBP: float
-    heart_cholesterol: float
-    avg_MaxHR: float
-    avg_Oldpeak: float
-    heart_disease_rate: float
-    gender: str
-    ever_married: str
-    work_type: str
-    Residence_type: str
-    smoking_status: str
-    smoke_flag: int
-    alco_flag: int
+    chest_pain: int
+    shortness_of_breath: int
+    irregular_heartbeat: int
+    fatigue_weakness: int
+    dizziness: int
+    swelling_edema: int
+    neck_jaw_shoulder_back_pain: int
+    excessive_sweating: int
+    persistent_cough: int
+    nausea_vomiting: int
+    high_blood_pressure: int
+    chest_discomfort_activity: int
+    cold_hands_feet: int
+    snoring_sleep_apnea: int
+    anxiety_feeling_of_doom: int
+    age: int 
 
-    model_config = ConfigDict(
-        json_schema_extra={
-            "example": {
-                "age": 50,
-                "hypertension": 0,
-                "heart_disease": 0,
-                "avg_glucose_level": 69.92,
-                "bmi": 18.7,
-                "avg_RestingBP": 125.71,
-                "heart_cholesterol": 219.28,
-                "avg_MaxHR": 140.57,
-                "avg_Oldpeak": 0.52,
-                "heart_disease_rate": 0.28,
-                "gender": "Female",
-                "ever_married": "Yes",
-                "work_type": "Self-employed",
-                "Residence_type": "Urban",
-                "smoking_status": "formerly smoked",
-                "smoke_flag": 1,
-                "alco_flag": 0
-            }
-        }
-    )
-
-# Redirect to Swagger Docs
 @app.get("/", tags=["Home"])
 def root():
     return RedirectResponse(url="/docs")
-
-# Health Check Endpoint
+    
 @app.get("/health", tags=["Health Check"])
 def health():
-    return {
+    return{
         "message": "Stroke Prediction System Running Successfully"
     }
 
-# Prediction Endpoint
 @app.post("/stroke-prediction", tags=["Prediction"])
 def predict(stroke: Stroke):
+    data = pd.DataFrame([{
+    "Chest Pain": stroke.chest_pain,
+    "Shortness of Breath": stroke.shortness_of_breath,
+    "Irregular Heartbeat": stroke.irregular_heartbeat,
+    "Fatigue & Weakness": stroke.fatigue_weakness,
+    "Dizziness": stroke.dizziness,
+    "Swelling (Edema)": stroke.swelling_edema,
+    "Pain in Neck/Jaw/Shoulder/Back": stroke.neck_jaw_shoulder_back_pain,
+    "Excessive Sweating": stroke.excessive_sweating,
+    "Persistent Cough": stroke.persistent_cough,
+    "Nausea/Vomiting": stroke.nausea_vomiting,
+    "High Blood Pressure": stroke.high_blood_pressure,
+    "Chest Discomfort (Activity)": stroke.chest_discomfort_activity,
+    "Cold Hands/Feet": stroke.cold_hands_feet,
+    "Snoring/Sleep Apnea": stroke.snoring_sleep_apnea,
+    "Anxiety/Feeling of Doom": stroke.anxiety_feeling_of_doom,
+    "Age": stroke.age
+}])
+    
+    prediction = int(model.predict(data)[0])
+    probability = float(model.predict_proba(data)[0][1])
 
-    try:
-        data = pd.DataFrame([{
-            "age": stroke.age,
-            "hypertension": stroke.hypertension,
-            "heart_disease": stroke.heart_disease,
-            "avg_glucose_level": stroke.avg_glucose_level,
-            "bmi": stroke.bmi,
-            "avg_RestingBP": stroke.avg_RestingBP,
-            "heart_cholesterol": stroke.heart_cholesterol,
-            "avg_MaxHR": stroke.avg_MaxHR,
-            "avg_Oldpeak": stroke.avg_Oldpeak,
-            "heart_disease_rate": stroke.heart_disease_rate,
-            "gender": stroke.gender,
-            "ever_married": stroke.ever_married,
-            "work_type": stroke.work_type,
-            "Residence_type": stroke.Residence_type,
-            "smoking_status": stroke.smoking_status,
-            "smoke_flag": stroke.smoke_flag,
-            "alco_flag": stroke.alco_flag
-        }])
-
-        prediction = int(model.predict(data)[0])
-
-        probability = float(
-            model.predict_proba(data)[0][1]
-        )
-
-        risk_level = (
-            "High Stroke Risk"
-            if prediction == 1
-            else "Low Stroke Risk"
-        )
-
-        return {
-            "prediction": prediction,
-            "risk_level": risk_level,
-            "stroke_probability_percent": round(probability * 100, 2),
-            "status": "Success"
-        }
-
-    except Exception as e:
-        return {
-            "status": "Error",
-            "message": str(e)
-        }
+    risk_level = (
+    "High Stroke Risk"
+    if prediction == 1
+    else "Low Stroke Risk"
+)
+    
+    return {
+        "status": "Success",
+        "prediction": prediction,
+        "risk_level": risk_level,
+        "stroke_probability_percent": round(probability * 100, 2)
+    }
